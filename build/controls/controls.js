2f0c996ac5f648a39a7c6cc9e8906ff761fd6fdb
Adam Biagianti <adam.biagianti@gmail.com>
2014-10-13 23:42:52 -0700

    react not for this project

--- build/controls/controls.js
+++ build/controls/controls.js
@@ -1,12 +1,29 @@
 /** @jsx React.DOM */
 
 var RangeControl = React.createClass({displayName: 'RangeControl',
+  getInitialState: function() {
+    return {
+      value: this.props.data.value
+    };
+  },
+
+  handleInput: function(event) {
+    this.setState({value: event.target.value});
+  },
+
   render: function() {
-    debugger
+    var data = this.props.data
     return (
       React.DOM.div({className: "col-1-3"}, 
-        React.DOM.label({htmlFor: this.props.name}, this.props.label.text), 
-        this.props.element
+        React.DOM.label({className: "m-bottom-10", htmlFor: data.name}, data.label.text), 
+        React.DOM.input({type: "range", 
+               onChange: this.handleInput, 
+               name: data.name, 
+               min: data.min, 
+               max: data.max, 
+               step: data.step, 
+               defaultValue: data.value}), 
+        React.DOM.span(null, "current value: ", this.state.value)
       )
     );
   }
@@ -16,9 +33,46 @@ var controls = [
   {
     type: 'range',
     name: "x-axis",
-    element: React.DOM.input({htmlType: "range", htmlName: "x-axis", htmlMin: "0", htmlMax: "360", htmlStep: "1", htmlValue: "2"}),
+    min: "0",
+    max: "360",
+    step: "1",
+    value: "2",
     label: {
       text: "x-axis degree"
+    }
+  },
+
+  {
+    type: 'range',
+    name: "y-axis",
+    min: "0",
+    max: "360",
+    step: "1",
+    value: "3",
+    label: {
+      text: "y-axis degree"
+    }
+  },
+  {
+    type: 'range',
+    name: "scale",
+    min: "0",
+    max: "200",
+    step: "0.5",
+    value: "4.5",
+    label: {
+      text: "scale"
+    }
+  },
+  {
+    type: 'range',
+    name: "speed",
+    min: "0",
+    max: "30",
+    step: "0.1",
+    value: "0.1",
+    label: {
+      text: "speed"
     }
   }
 ];
@@ -28,19 +82,26 @@ var controlTypes = {
 };
 
 var ControlGroup = React.createClass({displayName: 'ControlGroup',
+  componentDidMount: function() {
+    console.log(arguments);
+  },
+
 	render: function() {
     var _controls = [];
+
     this.props.controls.forEach(function(control,i) {
-      _controls[i] = controlTypes[control.type]({name: control.name, label: control.label, element: control.element});
+      controls.key = i;
+      _controls[i] = controlTypes[control.type]({
+        data: control,
+        onChange: this.test
+      });
     });
 
     return (
-      React.DOM.div(null, controls)
+      React.DOM.div(null, _controls)
     );
   }
 });
-
-
 
 
 
--- js/controls/controls.js
+++ js/controls/controls.js
@@ -1,12 +1,29 @@
 /** @jsx React.DOM */
 
 var RangeControl = React.createClass({
+  getInitialState: function() {
+    return {
+      value: this.props.data.value
+    };
+  },
+
+  handleInput: function(event) {
+    this.setState({value: event.target.value});
+  },
+
   render: function() {
-    debugger
+    var data = this.props.data
     return (
       <div className="col-1-3">
-        <label htmlFor={this.props.name}>{this.props.label.text}</label>
-        {this.props.element}
+        <label className="m-bottom-10" htmlFor={data.name}>{data.label.text}</label>
+        <input type="range"
+               onChange={this.handleInput}
+               name={data.name}
+               min={data.min}
+               max={data.max}
+               step={data.step}
+               defaultValue={data.value} />
+        <span>current value: {this.state.value}</span>
       </div>
     );
   }
@@ -16,9 +33,46 @@ var controls = [
   {
     type: 'range',
     name: "x-axis",
-    element: <input htmlType="range" htmlName="x-axis" htmlMin="0" htmlMax="360" htmlStep="1" htmlValue="2" />,
+    min: "0",
+    max: "360",
+    step: "1",
+    value: "2",
     label: {
       text: "x-axis degree"
+    }
+  },
+
+  {
+    type: 'range',
+    name: "y-axis",
+    min: "0",
+    max: "360",
+    step: "1",
+    value: "3",
+    label: {
+      text: "y-axis degree"
+    }
+  },
+  {
+    type: 'range',
+    name: "scale",
+    min: "0",
+    max: "200",
+    step: "0.5",
+    value: "4.5",
+    label: {
+      text: "scale"
+    }
+  },
+  {
+    type: 'range',
+    name: "speed",
+    min: "0",
+    max: "30",
+    step: "0.1",
+    value: "0.1",
+    label: {
+      text: "speed"
     }
   }
 ];
@@ -28,19 +82,29 @@ var controlTypes = {
 };
 
 var ControlGroup = React.createClass({
+  componentDidMount: function() {
+    console.log(arguments);
+  },
+  test: function() [
+
+  console.log('hi')
+},
 	render: function() {
     var _controls = [];
+
     this.props.controls.forEach(function(control,i) {
-      _controls[i] = controlTypes[control.type]({name: control.name, label: control.label, element: control.element});
+      controls.key = i;
+      _controls[i] = controlTypes[control.type]({
+        data: control,
+        onChange: this.test
+      });
     });
 
     return (
-      <div>{controls}</div>
+      <div>{_controls}</div>
     );
   }
 });
-
-
 
 
 
